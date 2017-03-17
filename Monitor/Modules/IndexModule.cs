d76634d83eade590da2ad3a25ae7fdf3192c93f1
Patryk <Matwijczuk>
2017-03-17 22:32:07 +0100

    Init Nancy project

---
+++ Monitor/Modules/IndexModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace Monitor.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters => "Its working!!!";
+        }
+    }
+}
