cb542ba87bdbd0aec61ca1cae2f942b3376a90c6
kpkozak <karol@kkozak.pl>
2017-03-29 20:51:32 +0200

    Configure Autofac to automatically register all types

---
+++ Monitor/Modules/Index/IndexModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+
+namespace Monitor.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        private readonly IRepeater _repeater;
+
+        public IndexModule(IRepeater repeater)
+        {
+            _repeater = repeater;
+            Get["/"] = parameters => "Its working!!!";
+            Get["/{value}"] = parameters => _repeater.Repeat(parameters.value);
+        }
+    }
+}
