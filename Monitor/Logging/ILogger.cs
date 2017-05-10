de47ba0b1aad72b0a27941dff72162649f2fcc06
kpkozak <karolpkozak@gmail.com>
2017-05-10 19:55:42 +0200

    Add simple logger logging to console

---
+++ Monitor/Logging/ILogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Monitor.Logging
+{
+    internal interface ILogger
+    {
+        void LogDebug(string message);
+        void LogInfo(string message);
+        void LogError(string message, Exception exception);
+    }
+}
---
+++ Monitor/Logging/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Monitor.Logging
+{
+    internal class Logger : ILogger
+    {
+        public void LogDebug(string message)
+        {
+            Console.Out.WriteLine($"{DateTime.Now} [DEBUG] {message}");
+        }
+
+        public void LogInfo(string message)
+        {
+            Console.Out.WriteLine($"{DateTime.Now} [INFO]  {message}");
+        }
+
+        public void LogError(string message, Exception exception)
+        {
+            Console.Out.WriteLine($"{DateTime.Now} [ERROR] {message}{Environment.NewLine}{exception}");
+        }
+    }
+}
